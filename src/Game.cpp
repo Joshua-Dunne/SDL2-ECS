7e0863de41445c937ea109abdacba4c1287e3e14
Joshua Dunne <c00241588@itcarlow.ie>
2021-11-12 22:00:21 +0000

    Changed Window Name for EXE.

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,7 +23,7 @@ void Game::run()
     else
     {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        window = SDL_CreateWindow( "Entity Component System", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         if( window == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
